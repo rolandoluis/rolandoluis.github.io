155831a3e277f1ae58a20cbdbe8d245b3662c94c
Rolando Luis Martin <rollmartin66@gmail.com>
2026-03-02 20:16:28 +0000

    cv: Efectos en Cards y Mejora del fondo

--- assets/js/main.js
+++ assets/js/main.js
@@ -326,3 +326,24 @@
     a.addEventListener("click", () => close());
   });
 })();
+
+// --- Card spotlight ------------------------------------
+(() => {
+
+  document.querySelectorAll(".card").forEach(card => {
+
+    card.addEventListener("mousemove", e => {
+
+      const rect = card.getBoundingClientRect();
+
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      card.style.setProperty("--x", x + "px");
+      card.style.setProperty("--y", y + "px");
+
+    });
+
+  });
+
+})();
